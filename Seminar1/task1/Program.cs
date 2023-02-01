fa5a1d4bdb42f93a47db77948800303fc4ccbbdf
Andrei <azhalevich@gmail.com>
2023-02-01 18:28:35 +0300

    Добавили задачи

---
+++ Seminar1/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает число и выдаёт его квадрат
+
+Console.Clear();
+
+Console.Write("ВВедите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
---
+++ Seminar1/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+ Console.Write("ВВедите первое число : ");
+int numberA = int.Parse(Console.ReadLine()!);
+ Console.Write("ВВедите второе число : ");
+int numberB = int.Parse(Console.ReadLine()!);
+if (numberB*numberB == numberA)
+{
+    Console.WriteLine($"Второе число {numberB} является квадратом первого числа {numberA}");
+}
+else{
+     Console.WriteLine($"Второе число {numberB} не является квадратом первого числа {numberA}");
+}
